2e6d95bbe3ebc021a5723c2385bea6c5bb32e9d8
Bailey Nepe <baileynepe@developersinstitute.co.nz>
2021-09-05 08:02:18 +1200

    add timesheet reducer

---
+++ frontend/src/reducers/timeSheetReducer.js
@@ -0,0 +1,12 @@
+export const timesheetReducer = (state = { timesheet: [] }, action) => {
+  switch (action.type) {
+    case "TIMESHEET_REQUEST":
+      return { loading: true, timesheet: [] };
+    case "TIMESHEET_SUCCESS":
+      return { loading: false, timesheet: action.payload };
+    case "TIMESHEET_FAIL":
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { timeSheetReducer } from "./reducers/timeSheetReducer";
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+  timeSheet: timeSheetReducer,
+});
 
 const initialState = {};
 
