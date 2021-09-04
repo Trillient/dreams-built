a6d699f4b9a271261daa05cbcf9a28f8e5e90219
Bailey Nepe <baileynepe@developersinstitute.co.nz>
2021-09-05 08:05:55 +1200

    setup constants

--- frontend/src/reducers/timeSheetReducer.js
+++ frontend/src/reducers/timeSheetReducer.js
@@ -1,10 +1,12 @@
+import { TIMESHEET_REQUEST, TIMESHEET_SUCCESS, TIMESHEET_FAIL } from "../constants/timeSheetConstants";
+
 export const timesheetReducer = (state = { timesheet: [] }, action) => {
   switch (action.type) {
-    case "TIMESHEET_REQUEST":
+    case TIMESHEET_REQUEST:
       return { loading: true, timesheet: [] };
-    case "TIMESHEET_SUCCESS":
+    case TIMESHEET_SUCCESS:
       return { loading: false, timesheet: action.payload };
-    case "TIMESHEET_FAIL":
+    case TIMESHEET_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
