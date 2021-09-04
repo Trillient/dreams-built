63673d3bb96b187688effd286b42453cbd7d84ba
Bailey Nepe <baileynepe@developersinstitute.co.nz>
2021-09-05 07:50:28 +1200

    remove array brackets around middleware

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -8,6 +8,6 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware([...middleware])));
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
 
 export default store;
