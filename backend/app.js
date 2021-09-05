bc7abbfffa839df4fda81126cd80e7e0d129f989
Bailey Nepe <baileynepe@developersinstitute.co.nz>
2021-09-05 21:20:16 +1200

    setup

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,1 @@
-const app = require("./app");
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
---
+++ backend/server.js
@@ -0,0 +1,7 @@
+const app = require("./app");
+const { connect } = require("./database");
+connect();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
