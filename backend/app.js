89225b1cdd66851b290d696dd99b8f7d1ab4fd40
Bailey Nepe <baileynepe@developersinstitute.co.nz>
2021-11-19 19:16:37 +1300

    refactoring

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-import { errorHandler } from './middleware/errorMiddleware.js';
-import morgan from 'morgan';
 import dotenv from 'dotenv';
+
 // import timesheetRoutes from './routes/timeSheetRoutes.js';
 // import userRoutes from './routes/userRoutes.js';
+import { errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
@@ -12,20 +12,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// app.use('/api/timesheet', timesheetRoutes);
-
-app.get(
-  '/',
-  (req,
-  (res) => {
-    res.send('hello world');
-  })
-);
-
-app.use(errorHandler);
-
 if (process.env.NODE_ENV === 'development') {
+  import morgan from 'morgan';
   app.use(morgan('dev'));
 }
+
+// app.use('/api/timesheet', timesheetRoutes);
+
+app.get('/', (req, res) => {
+  res.json('API running...');
+});
+
+app.use(errorHandler);
 
 export default app;
--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -8,6 +8,7 @@ const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
   res.json({
+    statusCode: statusCode,
     message: err.message,
     stack: process.env.NODE_ENV === 'production' ? null : err.stack,
   });
