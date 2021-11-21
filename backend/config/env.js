549d610538b02780e1fefa889b5309fb9650221e
Bailey Nepe <baileynepe@developersinstitute.co.nz>
2021-11-21 20:47:59 +1300

    added authentication

---
+++ backend/config/env.js
@@ -0,0 +1,15 @@
+const audience = process.env.AUTH0_AUDIENCE;
+const domain = process.env.AUTH0_DOMAIN;
+
+if (!audience) {
+  throw new Error('Missing the definition of an AUDIENCE environmental variable');
+}
+
+if (!domain) {
+  throw new Error('Missing the definition of a DOMAIN environmental variable');
+}
+
+module.exports = {
+  audience,
+  domain,
+};
--- backend/routes/jobDetailsRoutes.js
+++ backend/routes/jobDetailsRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const { checkJwt } = require('../middleware/authMiddleware');
 const { getJobs, getJob, createJob, updateJob, deleteJob } = require('../controllers/jobDetailsController');
 
-router.route('/').get(getJobs).post(createJob);
-router.route('/:id').get(getJob).put(updateJob).delete(deleteJob);
+router.route('/').get(checkJwt, getJobs).post(checkJwt, createJob);
+router.route('/:id').get(checkJwt, getJob).put(checkJwt, updateJob).delete(checkJwt, deleteJob);
 
 module.exports = router;
