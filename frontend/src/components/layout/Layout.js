530a13d037698c0167581b5b647e38e3a0f26941
Bailey Nepe <baileynepe@developersinstitute.co.nz>
2022-01-06 10:20:37 +1300

    smooth authenticated transition

--- frontend/src/components/layout/Layout.js
+++ frontend/src/components/layout/Layout.js
@@ -8,33 +8,29 @@ import SidebarHeader from './SidebarHeader';
 import styles from './layout.module.css';
 
 const Layout = ({ children }) => {
-  const { isAuthenticated } = useAuth0();
+  const { isAuthenticated, isLoading } = useAuth0();
 
   const [sidebar, setSidebar] = useState(true);
 
-  if (isAuthenticated) {
-    return (
-      <div className={styles.transition}>
-        <div className={sidebar ? styles['side-menu-grid'] : styles['clean-menu-grid']}>
-          <div className={styles.header}>
-            <SidebarHeader setSidebar={setSidebar} sidebar={sidebar} />
-          </div>
-          <div className={styles.sidebar} style={sidebar ? null : { display: 'none' }}>
-            <Sidebar setSidebar={setSidebar} />
-          </div>
-          <div className={styles.main}>{children}</div>
+  return isLoading ? null : isAuthenticated ? (
+    <div className={styles.transition}>
+      <div className={sidebar ? styles['side-menu-grid'] : styles['clean-menu-grid']}>
+        <div className={styles.header}>
+          <SidebarHeader setSidebar={setSidebar} sidebar={sidebar} />
         </div>
-      </div>
-    );
-  } else {
-    return (
-      <div className={styles.grid}>
-        <Header />
+        <div className={styles.sidebar} style={sidebar ? null : { display: 'none' }}>
+          <Sidebar setSidebar={setSidebar} />
+        </div>
         <div className={styles.main}>{children}</div>
-        <Footer />
       </div>
-    );
-  }
+    </div>
+  ) : (
+    <div className={styles.grid}>
+      <Header />
+      <div className={styles.main}>{children}</div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
