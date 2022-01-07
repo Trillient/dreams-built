ea87e632c9d8749ba14644ea0061053baa551e40
Bailey Nepe <baileynepe@developersinstitute.co.nz>
2022-01-07 13:19:48 +1300

    removed model properties

--- backend/models/__tests__/userModel.test.js
+++ backend/models/__tests__/userModel.test.js
@@ -1,31 +1,9 @@
 const User = require('../userModel.js');
 
 describe('Given a User schema model has been updated', () => {
-  it("A property named 'firstName' should be on the schema", () => {
-    const firstName = User.schema.obj.firstName;
-    expect(firstName).toBeTruthy();
-    expect(firstName.type()).toEqual(expect.any(String));
+  it("A property named 'userId' should be on the schema", () => {
+    const userId = User.schema.obj.userId;
+    expect(userId).toBeTruthy();
+    expect(userId.type()).toEqual(expect.any(String));
   });
-
-  it("A property named 'lastName' should be on the schema", () => {
-    const lastName = User.schema.obj.lastName;
-    expect(lastName).toBeTruthy();
-    expect(lastName.type()).toEqual(expect.any(String));
-  });
-
-  it("A property named 'email' should be on the schema and is unique", () => {
-    const email = User.schema.obj.email;
-    expect(email).toBeTruthy();
-    expect(email.unique).toBeTruthy();
-  });
-
-  it('isAdmin should be of type Boolean and a default value of False', () => {
-    const isAdmin = User.schema.obj.isAdmin;
-    expect(isAdmin).toBeTruthy();
-    expect(isAdmin.default).toBeFalsy();
-  });
-
-  // TODO - user Details Schema test
-
-  // TODO - timestamp test
 });
