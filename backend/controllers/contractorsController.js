2fe5484384ff598d694b0bd4c6067a2a72bb5911
Bailey Nepe <baileynepe@developersinstitute.co.nz>
2022-01-31 15:35:41 +1300

    /api/contractors/:id GET tests

--- backend/controllers/contractorsController.js
+++ backend/controllers/contractorsController.js
@@ -12,9 +12,37 @@ const getContractors = asyncHandler(async (req, res) => {
   res.json(contractorList);
 });
 
-const createContractors = asyncHandler(async (req, res) => {});
+/**
+ * @Desc Create a Contractor
+ * @Route POST /api/contractors
+ * @Access Private ("create:contractors" permission, Admin)
+ */
 
-const getContractor = asyncHandler(async (req, res) => {});
+const createContractors = asyncHandler(async (req, res) => {
+  const { contractor, contact, email, phone } = req.body;
+
+  const contractorExists = await Contractor.find({ contractor: contractor });
+
+  if (contractorExists.length < 1) {
+    const createdContractor = await Contractor.create({ contractor: contractor, contact: contact, email: email, phone: phone });
+    res.status(201);
+    res.json({ message: 'success', contractor: createdContractor });
+  } else {
+    res.status(409);
+    throw new Error('Contractor already exists');
+  }
+});
+
+const getContractor = asyncHandler(async (req, res) => {
+  const contractor = await Contractor.findById(req.params.id);
+
+  if (contractor) {
+    res.json(contractor);
+  } else {
+    res.status(404);
+    throw new Error('Contractor not found');
+  }
+});
 
 const updateContractor = asyncHandler(async (req, res) => {});
 
--- backend/routes/__tests__/contractorsRoutes.test.js
+++ backend/routes/__tests__/contractorsRoutes.test.js
@@ -40,3 +40,348 @@ const createNewContractor = (contractor, contact, email, phone) => {
     phone: phone,
   };
 };
+
+describe('Given we have an "/api/contractors" endpoint', () => {
+  it('When a GET request is made and is valid, authenticated and appropriately authorized, then a 200 response with a list of contractors should be returned', async () => {
+    for (let i = 0; i < 20; i++) {
+      await Contractor.create(createNewContractor(`contractor${i}`));
+    }
+
+    const checkBody = (res) => {
+      expect(res.body.error).toBeUndefined();
+      expect(res.body.length).toBe(20);
+    };
+
+    const validToken = jwks.token({
+      aud: audience,
+      iss: `https://${domain}/`,
+      sub: clientId,
+      permissions: 'read:contractors',
+    });
+
+    await request(app)
+      .get('/api/contractors/')
+      .set(`Authorization`, `Bearer ${validToken}`)
+      .set('Content-Type', 'application/json')
+      .expect('Content-Type', /application\/json/)
+      .expect(checkBody)
+      .expect(200);
+  });
+  it('When a GET request has an invalid token, then a 401 response is returned', async () => {
+    for (let i = 0; i < 1; i++) {
+      await Contractor.create(createNewContractor(`contractor${i}`));
+    }
+
+    const checkBody = (res) => {
+      expect(res.body.code).toBe('invalid_token');
+    };
+
+    const invalidToken = jwks.token({
+      aud: audience,
+      iss: `https://{domain}/`,
+      sub: clientId,
+      permissions: 'read:contractors',
+    });
+
+    await request(app)
+      .get('/api/contractors/')
+      .set(`Authorization`, `Bearer ${invalidToken}`)
+      .set('Content-Type', 'application/json')
+      .expect('Content-Type', /application\/json/)
+      .expect(checkBody)
+      .expect(401);
+  });
+  it('When a GET request has insufficient permissions, then a 403 response is returned', async () => {
+    for (let i = 0; i < 1; i++) {
+      await Contractor.create(createNewContractor(`contractor${i}`));
+    }
+
+    const checkBody = (res) => {
+      expect(res.body.error).toBe('Forbidden');
+    };
+
+    const invalidToken = jwks.token({
+      aud: audience,
+      iss: `https://${domain}/`,
+      sub: clientId,
+    });
+
+    await request(app)
+      .get('/api/contractors/')
+      .set(`Authorization`, `Bearer ${invalidToken}`)
+      .set('Content-Type', 'application/json')
+      .expect('Content-Type', /application\/json/)
+      .expect(checkBody)
+      .expect(403);
+  });
+  it('When a POST request is made and is valid, authenticated and appropriately authorized, then a 201 response should be returned', async () => {
+    const contractor = createNewContractor('foomill', 'foo', 'foo@gmail.com', '555931888');
+
+    const checkBody = (res) => {
+      expect(res.body.message).toBe('success');
+      expect(res.body.contractor).toEqual(expect.objectContaining(contractor));
+    };
+
+    const validToken = jwks.token({
+      aud: audience,
+      iss: `https://${domain}/`,
+      sub: clientId,
+      permissions: 'create:contractors',
+    });
+
+    await request(app)
+      .post('/api/contractors/')
+      .send(contractor)
+      .set(`Authorization`, `Bearer ${validToken}`)
+      .set('Content-Type', 'application/json')
+      .expect('Content-Type', /application\/json/)
+      .expect(checkBody)
+      .expect(201);
+  });
+  it('When a POST request is made and has an invalid token, then a 401 response should be returned', async () => {
+    const contractor = createNewContractor('foomill', 'foo', 'foo@gmail.com', '555931888');
+
+    const checkBody = (res) => {
+      expect(res.body.code).toBe('invalid_token');
+    };
+
+    const invalidToken = jwks.token({
+      aud: audience,
+      iss: `https://{domain}/`,
+      sub: clientId,
+      permissions: 'create:contractors',
+    });
+
+    await request(app)
+      .post('/api/contractors/')
+      .send(contractor)
+      .set(`Authorization`, `Bearer ${invalidToken}`)
+      .set('Content-Type', 'application/json')
+      .expect('Content-Type', /application\/json/)
+      .expect(checkBody)
+      .expect(401);
+  });
+  it('When a POST request is made and has insufficient permissions, then a 403 response should be returned', async () => {
+    const contractor = createNewContractor('foomill', 'foo', 'foo@gmail.com', '555931888');
+
+    const checkBody = (res) => {
+      expect(res.body.error).toBe('Forbidden');
+    };
+
+    const invalidToken = jwks.token({
+      aud: audience,
+      iss: `https://${domain}/`,
+      sub: clientId,
+    });
+
+    await request(app)
+      .post('/api/contractors/')
+      .send(contractor)
+      .set(`Authorization`, `Bearer ${invalidToken}`)
+      .set('Content-Type', 'application/json')
+      .expect('Content-Type', /application\/json/)
+      .expect(checkBody)
+      .expect(403);
+  });
+  it('When a POST request is made and the contractor already exists, then a 409 response should be returned', async () => {
+    const contractor = createNewContractor('foomill', 'foo', 'foo@gmail.com', '555931888');
+
+    await Contractor.create(contractor);
+
+    const checkBody = (res) => {
+      expect(res.body.message).toBe('Contractor already exists');
+    };
+
+    await request(app)
+      .post('/api/contractors/')
+      .send(contractor)
+      .set(`Authorization`, `Bearer ${token}`)
+      .set('Content-Type', 'application/json')
+      .expect('Content-Type', /application\/json/)
+      .expect(checkBody)
+      .expect(409);
+  });
+  it('When a POST request is made and the "contractor" property is invalid, then a 400 response should be returned', async () => {
+    const contractor = createNewContractor(12345, 'foo', 'foo@gmail.com', '555931888');
+
+    const checkBody = (res) => {
+      expect(res.body.errors[0].msg).toBe('Contractor Invalid');
+    };
+
+    await request(app)
+      .post('/api/contractors/')
+      .send(contractor)
+      .set(`Authorization`, `Bearer ${token}`)
+      .set('Content-Type', 'application/json')
+      .expect('Content-Type', /application\/json/)
+      .expect(checkBody)
+      .expect(400);
+  });
+  it('When a POST request is made and the "contractor" property is missing, then a 400 response should be returned', async () => {
+    const contractor = createNewContractor(null, 'foo', 'foo@gmail.com', '555931888');
+
+    const checkBody = (res) => {
+      expect(res.body.errors[0].msg).toBe('Contractor Invalid');
+    };
+
+    await request(app)
+      .post('/api/contractors/')
+      .send(contractor)
+      .set(`Authorization`, `Bearer ${token}`)
+      .set('Content-Type', 'application/json')
+      .expect('Content-Type', /application\/json/)
+      .expect(checkBody)
+      .expect(400);
+  });
+  it('When a POST request is made and the "contact" property is invalid, then a 400 response should be returned', async () => {
+    const contractor = createNewContractor('foomill', 1234, 'foo@gmail.com', '555931888');
+
+    const checkBody = (res) => {
+      expect(res.body.errors[0].msg).toBe('Contact Invalid');
+    };
+
+    await request(app)
+      .post('/api/contractors/')
+      .send(contractor)
+      .set(`Authorization`, `Bearer ${token}`)
+      .set('Content-Type', 'application/json')
+      .expect('Content-Type', /application\/json/)
+      .expect(checkBody)
+      .expect(400);
+  });
+  it('When a POST request is made and the "email" property is invalid, then a 400 response should be returned', async () => {
+    const contractor = createNewContractor('foomill', 'foo', 'foogmail.com', '555931888');
+
+    const checkBody = (res) => {
+      expect(res.body.errors[0].msg).toBe('Email invalid');
+    };
+
+    await request(app)
+      .post('/api/contractors/')
+      .send(contractor)
+      .set(`Authorization`, `Bearer ${token}`)
+      .set('Content-Type', 'application/json')
+      .expect('Content-Type', /application\/json/)
+      .expect(checkBody)
+      .expect(400);
+  });
+  it('When a POST request is made and the "phone" property is invalid, then a 400 response should be returned', async () => {
+    const contractor = createNewContractor('foomill', 'foo', 'foo@gmail.com', { phone: 222222 });
+
+    const checkBody = (res) => {
+      expect(res.body.errors[0].msg).toBe('Phone invalid');
+    };
+
+    await request(app)
+      .post('/api/contractors/')
+      .send(contractor)
+      .set(`Authorization`, `Bearer ${token}`)
+      .set('Content-Type', 'application/json')
+      .expect('Content-Type', /application\/json/)
+      .expect(checkBody)
+      .expect(400);
+  });
+});
+
+describe('Given we have an "/api/contractors/:id" endpoint', () => {
+  beforeEach(async () => {
+    await Contractor.create(createNewContractor('fooMill', 'foo', 'foo@gmail.com', '1122222'));
+  });
+  it("When a GET request is made and is valid, authenticated and appropriately authorized, then a 200 response with a contractor's details should be returned", async () => {
+    const contractor = await Contractor.findOne({ contractor: 'fooMill' });
+    const contractorId = contractor._id;
+
+    const checkBody = (res) => {
+      expect(res.body.contractor).toBe('fooMill');
+      expect(res.body.contact).toBe('foo');
+      expect(res.body.email).toBe('foo@gmail.com');
+      expect(res.body.phone).toBe('1122222');
+    };
+
+    const validToken = jwks.token({
+      aud: audience,
+      iss: `https://${domain}/`,
+      sub: clientId,
+      permissions: 'read:contractors',
+    });
+
+    await request(app)
+      .get(`/api/contractors/${contractorId}`)
+      .set(`Authorization`, `Bearer ${validToken}`)
+      .set('Content-Type', 'application/json')
+      .expect('Content-Type', /application\/json/)
+      .expect(checkBody)
+      .expect(200);
+  });
+  it('When a GET request is made and has an invalid token, then a 401 response should be returned', async () => {
+    const contractor = await Contractor.findOne({ contractor: 'fooMill' });
+    const contractorId = contractor._id;
+
+    const checkBody = (res) => {
+      expect(res.body.code).toBe('invalid_token');
+    };
+
+    const invalidToken = jwks.token({
+      aud: audience,
+      iss: `https://{domain}/`,
+      sub: clientId,
+      permissions: 'read:contractors',
+    });
+
+    await request(app)
+      .get(`/api/contractors/${contractorId}`)
+      .set(`Authorization`, `Bearer ${invalidToken}`)
+      .set('Content-Type', 'application/json')
+      .expect('Content-Type', /application\/json/)
+      .expect(checkBody)
+      .expect(401);
+  });
+  it('When a GET request is made and has insufficient permissions, then a 403 response should be returned', async () => {
+    const contractor = await Contractor.findOne({ contractor: 'fooMill' });
+    const contractorId = contractor._id;
+
+    const checkBody = (res) => {
+      expect(res.body.error).toBe('Forbidden');
+    };
+
+    const invalidToken = jwks.token({
+      aud: audience,
+      iss: `https://${domain}/`,
+      sub: clientId,
+    });
+
+    await request(app)
+      .get(`/api/contractors/${contractorId}`)
+      .set(`Authorization`, `Bearer ${invalidToken}`)
+      .set('Content-Type', 'application/json')
+      .expect('Content-Type', /application\/json/)
+      .expect(checkBody)
+      .expect(403);
+  });
+  it('When a GET request is made and has an invalid "id" parameter, then a 400 response should be returned', async () => {
+    const checkBody = (res) => {
+      expect(res.body.errors[0].msg).toBe('Invalid contractor');
+    };
+
+    await request(app)
+      .get(`/api/contractors/invalididparam`)
+      .set(`Authorization`, `Bearer ${token}`)
+      .set('Content-Type', 'application/json')
+      .expect('Content-Type', /application\/json/)
+      .expect(checkBody)
+      .expect(400);
+  });
+  it('When a GET request is made and has an "id" parameter that does not exist, then a 404 response should be returned', async () => {
+    const checkBody = (res) => {
+      expect(res.body.message).toBe('Contractor not found');
+    };
+
+    await request(app)
+      .get(`/api/contractors/507f191e810c19729de860ea`)
+      .set(`Authorization`, `Bearer ${token}`)
+      .set('Content-Type', 'application/json')
+      .expect('Content-Type', /application\/json/)
+      .expect(checkBody)
+      .expect(404);
+  });
+});
