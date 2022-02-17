20a7460024b31a0cb02d037be796436b64c97b38
Bailey Nepe <baileynepe@developersinstitute.co.nz>
2022-02-18 04:33:48 +1300

    change modal sizes

--- frontend/src/components/modals/ScheduleCreateDueDate.js
+++ frontend/src/components/modals/ScheduleCreateDueDate.js
@@ -2,7 +2,7 @@ import { Button, Modal } from 'react-bootstrap';
 
 const ScheduleCreateDueDate = ({ setModalShow, date, jobPart, ...rest }) => {
   return (
-    <Modal {...rest} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+    <Modal {...rest} size="md" aria-labelledby="contained-modal-title-vcenter" centered>
       <Modal.Header>
         <Modal.Title id="contained-modal-title-vcenter"></Modal.Title>
         <Button variant="secondary" onClick={() => setModalShow(false)}>
--- frontend/src/components/modals/ScheduleEditDueDate.js
+++ frontend/src/components/modals/ScheduleEditDueDate.js
@@ -2,7 +2,7 @@ import { Button, Modal } from 'react-bootstrap';
 
 const ScheduleEditDueDate = ({ setModalShow, date, job, jobPart, ...rest }) => {
   return (
-    <Modal {...rest} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+    <Modal {...rest} size="md" aria-labelledby="contained-modal-title-vcenter" centered>
       <Modal.Header>
         <Modal.Title id="contained-modal-title-vcenter"></Modal.Title>
         <Button variant="secondary" onClick={() => setModalShow(false)}>
