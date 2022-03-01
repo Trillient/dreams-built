4e731c7d2c9f4e63d5dda3ef874d05dbbbd4a5b6
Bailey Nepe <baileynepe@developersinstitute.co.nz>
2022-03-02 08:27:36 +1300

    handle auth0 email chnage

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -98,13 +98,14 @@ const getUser = asyncHandler(async (req, res) => {
  */
 
 const updateUser = asyncHandler(async (req, res) => {
-  const { firstName, lastName, hourlyRate } = req.body;
+  const { firstName, lastName, auth0Email, hourlyRate } = req.body;
 
   const user = await User.findById(req.params.id);
 
   if (user) {
     user.firstName = firstName;
     user.lastName = lastName;
+    user.auth0Email = auth0Email || user.auth0Email;
     user.hourlyRate = hourlyRate;
 
     await user.save();
