0a91b1ec4da34d67fc67151d9664c414d5cefadc
Bailey Nepe <baileynepe@developersinstitute.co.nz>
2022-03-05 09:49:56 +1300

    prep for intro video

--- frontend/src/screens/homeScreen/HomeScreen.js
+++ frontend/src/screens/homeScreen/HomeScreen.js
@@ -5,20 +5,20 @@ import styles from './homeScreen.module.css';
 const HomeScreen = () => {
   return (
     <Container>
-      <section>Intro Video</section>
+      {/* <section>Intro Video</section>
       <section>About the app + Login credentials</section>
       <section>
         <h2>Features</h2>
         <Carousel fade>
           <Carousel.Item>
-            {/* <img className="d-block w-100"  alt="First slide" /> */}
+            <img className="d-block w-100"  alt="First slide" />
             <Carousel.Caption>
               <h3>First slide label</h3>
               <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            {/* <img className="d-block w-100" src="holder.js/800x400?text=Second slide&bg=282c34" alt="Second slide" /> */}
+            <img className="d-block w-100" src="holder.js/800x400?text=Second slide&bg=282c34" alt="Second slide" />
 
             <Carousel.Caption>
               <h3>Second slide label</h3>
@@ -26,7 +26,7 @@ const HomeScreen = () => {
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            {/* <img className="d-block w-100" src="holder.js/800x400?text=Third slide&bg=20232a" alt="Third slide" /> */}
+            <img className="d-block w-100" src="holder.js/800x400?text=Third slide&bg=20232a" alt="Third slide" />
 
             <Carousel.Caption>
               <h3>Third slide label</h3>
@@ -35,7 +35,7 @@ const HomeScreen = () => {
           </Carousel.Item>
         </Carousel>
       </section>
-      <section>Contact</section>
+      <section>Contact</section> */}
     </Container>
   );
 };
